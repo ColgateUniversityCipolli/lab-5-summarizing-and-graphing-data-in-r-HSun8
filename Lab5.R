032ec2ef26ccd773bdb8055b2afd8a4e975fabd1
HSun8 <24sunh@gmail.com>
2025-02-23 21:32:09 -0500

    step 1 polishing, working on step 2

--- Lab5.R
+++ Lab5.R
@@ -5,28 +5,52 @@ library("tidyverse")
 # load in data
 essentia.data = read_csv("data/essentia.data.csv")
 allentown.data = read_csv("data/essentia.data.allentown.csv")
-# rangetest <- function(feature){
-feature <- "overall_loudness"
+# function
+rangetest <- function(feature){
+  #feature <- "overall_loudness"
   essentia.summary <- essentia.data |>
-  # group data by artist
+    # group data by artist
     group_by(artist) |>
-  # summarize artist with min, LF, UF, max
+    # summarize artist with min, LF, UF, max
     summarize(minimum = min(get(feature)),
               LF = quantile(get(feature), 0.25) - 1.5 * IQR(get(feature)),
               UF = quantile(get(feature), 0.75) + 1.5 * IQR(get(feature)),
               maximum = max(get(feature))) |>
-      # out.of.range
-      mutate(out.of.range = if_else(allentown.data[[feature]] < minimum | 
+    # out.of.range
+    mutate(out.of.range = if_else(allentown.data[[feature]] < minimum | 
                                     allentown.data[[feature]] > maximum, 
-                                    TRUE, FALSE)) |>
-      # unsual
-      mutate(unusual = if_else(allentown.data[[feature]] < LF | 
+                                  TRUE, FALSE)) |>
+    # unsual
+    mutate(unusual = if_else(allentown.data[[feature]] < LF | 
                                allentown.data[[feature]] > UF,
-                               TRUE, FALSE)) |>
-      mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
-                                     unusual == TRUE ~ "Outlying",
-                                     .default = "Within Range"))
-#}
+                             TRUE, FALSE)) |>
+    mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
+                                   unusual == TRUE ~ "Outlying",
+                                   .default = "Within Range")) |>
+    select(all_of(c("artist", "out.of.range", "unusual", "description")))
+  # return tibble 
+  essentia.summary
+}
+
+# Step 2
+all.features = c(names(allentown.data))
+# numeric data
+numeric.cols <- essentia.data |> 
+  select(where(is.numeric)) 
+# categorical data
+cat.cols <- essentia.data |>
+  select(!where(is.numeric))
+# apply function to all numeric data
+numeric.names = names(numeric.cols)
+for (i in 1:numeric.names){
+  
+}
+  
+  
+
+  
+
+
 
     
         
