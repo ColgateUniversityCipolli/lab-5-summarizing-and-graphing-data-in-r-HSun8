06d48633094ec037b713654be879d6aded26a297
HSun8 <24sunh@gmail.com>
2025-02-23 19:57:57 -0500

    step 1 finished (i was actually not that close before)

--- Lab5.R
+++ Lab5.R
@@ -2,22 +2,31 @@
 # Henry Sun 
 library("tidyverse")
 # Step 1
+# load in data
 essentia.data = read_csv("data/essentia.data.csv")
-#rangetest <- function(feature)
-  feature <- "overall.loudness"
-  essentia.data|>
+allentown.data = read_csv("data/essentia.data.allentown.csv")
+# rangetest <- function(feature){
+feature <- "overall_loudness"
+  essentia.summary <- essentia.data |>
+  # group data by artist
     group_by(artist) |>
-    summarize(min = min(overall_loudness),
-              LF = quantile(overall_loudness, 0.25) - 1.5 * IQR(overall_loudness),
-              UF = quantile(overall_loudness, 0.75) + 1.5 * IQR(overall_loudness),
-              max = max(overall_loudness)) |>
-    mutate(out.of.range = if_else(overall_loudness < min | overall_loudness > max, 
-                                  TRUE, FALSE)) |>
-    mutate(unusual = if_else(overall_loudness < LF | overall_loudness > UF, 
-                             TRUE, FALSE))
-    mutate(description = if_else(out.of.range == TRUE | out.of.range == TRUE, 
-                                 "Out of Range", "Within Range"))
-    
+  # summarize artist with min, LF, UF, max
+    summarize(minimum = min(get(feature)),
+              LF = quantile(get(feature), 0.25) - 1.5 * IQR(get(feature)),
+              UF = quantile(get(feature), 0.75) + 1.5 * IQR(get(feature)),
+              maximum = max(get(feature))) |>
+      # out.of.range
+      mutate(out.of.range = if_else(allentown.data[[feature]] < minimum | 
+                                    allentown.data[[feature]] > maximum, 
+                                    TRUE, FALSE)) |>
+      # unsual
+      mutate(unusual = if_else(allentown.data[[feature]] < LF | 
+                               allentown.data[[feature]] > UF,
+                               TRUE, FALSE)) |>
+      mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
+                                     unusual == TRUE ~ "Outlying",
+                                     .default = "Within Range"))
+#}
 
     
-          
+        
